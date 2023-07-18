5552d0997a42a316d18ef22537a5103785a6cfd3
unknown <lesnarlogesh1232gmail.com>
2023-07-18 23:18:08 +0530

    Adding battery Functions

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,15 @@
+from battery.battery import Battery
+from datetime import datetime
+today = datetime.today().date()
+
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        date_which_battery_should_be_serviced_by = today.replace(year=today.year)(self.last_service_date, 4)
+        if date_which_battery_should_be_serviced_by < self.current_date:
+            return True
+        else:
+            return False
